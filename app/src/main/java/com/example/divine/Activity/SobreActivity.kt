11276a45e7ae3fca325414383f5faa5225f1bd1a
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-05-16 02:43:38 -0300

    classe para informar  que o app propõem usando a API aboutpage

---
+++ app/src/main/java/com/example/divine/Activity/SobreActivity.kt
@@ -0,0 +1,29 @@
+package com.example.divine.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.divine.R
+import mehdi.sakout.aboutpage.AboutPage
+
+class SobreActivity : AppCompatActivity() {
+
+//    não utiliza o layout activity_sobre
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val description = "Um projeto para desenvolver as habilidade e os conhecimentos aprendido ao decorre dos estudos com kotlin.\n\n"+
+"Nome_do_Jogo ele serve para instigar a capacidade motora e reflexiva do jogador ao desafiá-lo a descobrir quais imagens são referentes a quais filmes em pouco tempo,"+
+"com a dificuldade de um teclado embaralhado. Tem o intuito de ser um jogo divertido e instigante."
+        val sobre: View = AboutPage(this)
+            .setImage(R.drawable.designhome3)
+            .setDescription(description)
+            .addGroup("Fale Conosco")
+            .addEmail("emanoel.silva@dce.ufpb.br","Envie um e-mail")
+            .addGroup("Rede Social")
+            .addGitHub("emanoelmlsilva?tab=repositories","Outros Prjetos")
+            .create()
+        setContentView(sobre)
+    }
+}
