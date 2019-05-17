2cb1e3d9ac68be4d6a6b6a479fbfbdcc538d84fe
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-05-17 18:18:46 -0300

    adição do click do botão de recordes e refatoramento do metodo de click de botão

--- app/src/main/java/com/example/divine/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/divine/Fragments/HomeFragment.kt
@@ -12,20 +12,28 @@ import com.example.divine.R
 class HomeFragment : Fragment() {
 
     var buttonFilmes: ImageButton? = null
+    var buttonRecords: ImageButton? = null
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         var view: View = inflater.inflate(R.layout.fragment_home, container, false)
         buttonFilmes = view.findViewById(R.id.btn_gameStart)
-        openStart()
+        buttonRecords = view.findViewById(R.id.btn_records)
+        openButtons()
         return view
     }
 
-    fun openStart(){
+    fun openButtons(){
         buttonFilmes?.setOnClickListener(View.OnClickListener {
             var fragmentTransaction: FragmentTransaction = fragmentManager!!.beginTransaction()
             fragmentTransaction.replace(R.id.frameChoose,FasesFragment())
+            fragmentTransaction.commit()
+        })
+
+        buttonRecords?.setOnClickListener(View.OnClickListener {
+            var fragmentTransaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+            fragmentTransaction.replace(R.id.frameChoose,RecordsFragment())
             fragmentTransaction.commit()
         })
     }
