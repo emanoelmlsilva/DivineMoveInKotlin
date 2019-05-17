b206e67fcaf41ba6b2744b6628b03ee3f875b94a
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-05-16 22:13:48 -0300

    refatoramento no nome do botao

--- app/src/main/java/com/example/divine/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/divine/Fragments/HomeFragment.kt
@@ -6,10 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.divine.R
-import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -19,12 +17,13 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         var view: View = inflater.inflate(R.layout.fragment_home, container, false)
+        buttonFilmes = view.findViewById(R.id.btn_gameStart)
         openStart()
         return view
     }
 
     fun openStart(){
-        btn_game.setOnClickListener(View.OnClickListener {
+        buttonFilmes?.setOnClickListener(View.OnClickListener {
             var fragmentTransaction: FragmentTransaction = fragmentManager!!.beginTransaction()
             fragmentTransaction.replace(R.id.frameChoose,FasesFragment())
             fragmentTransaction.commit()
