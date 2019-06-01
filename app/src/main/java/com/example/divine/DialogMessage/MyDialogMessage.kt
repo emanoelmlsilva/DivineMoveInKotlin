fe3a7c6a9776ab732be0d1d8e0920304a4db820d
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-06-01 17:49:42 -0300

    refatoramento nos metodos de message

--- app/src/main/java/com/example/divine/DialogMessage/MyDialogMessage.kt
+++ app/src/main/java/com/example/divine/DialogMessage/MyDialogMessage.kt
@@ -2,9 +2,11 @@ package com.example.divine.DialogMessage
 
 import android.content.Context
 import android.content.DialogInterface
+import android.util.Log
 import android.widget.Chronometer
 import androidx.appcompat.app.AlertDialog
 import com.example.divine.Chronometer.MyChronometer
+import com.example.divine.R
 
 class MyDialogMessage(myContext: Context, idStyle: Int) {
 
@@ -15,28 +17,31 @@ class MyDialogMessage(myContext: Context, idStyle: Int) {
     }
 
     fun message(title: String,back: BackToMenu){
-
+        Log.i("mensagem","escohida o normal")
         builder.setTitle(title)
 //        configurar cancelamento
         builder.setCancelable(false)
 //        configurar icone
         builder.setIcon(android.R.drawable.ic_dialog_info)
 //        configurar opções de butões sim ou não
-        builder.setPositiveButton("ok",DialogInterface.OnClickListener { dialog, which -> back.popBack() })
-
+        builder.setPositiveButton("Ok",DialogInterface.OnClickListener { dialog, which -> back.popBack() })
+        builder.setNegativeButton("",DialogInterface.OnClickListener { dialog, whick -> null})
         builder.create()
         builder.show()
 
     }
 
-    fun messageCheckVictor(title: String,id: Int){
+    fun messageCheckExit(title: String,back: BackToMenu){
+        Log.i("mensagem","escohida o exit")
         builder.setTitle(title)
 //        configurar cancelamento
-        builder.setCancelable(true)
+        builder.setCancelable(false)
 //        configurar icone
-        builder.setIcon(id)
+        builder.setIcon(R.drawable.alert_box)
 //configurar opção de sim ou não
-        builder.setPositiveButton(" ",DialogInterface.OnClickListener {dialog, which -> })//função lambda
+        builder.setPositiveButton("Ok",DialogInterface.OnClickListener {dialog, which -> back.popBack() })
+        builder.setNegativeButton("Cancel",DialogInterface.OnClickListener{dialog, which -> })
+
         builder.create()
         builder.show()
     }
